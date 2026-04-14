241a113fb2577fe5d142c12162a7bb0622cc6736
nabbang6 <hawwah03@gachon.ac.kr>
2026-04-14 16:22:18 +0900

    feat: SVG 아이콘 에셋 및 barrel export 추가

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as CircleAlertIcon } from "./ic-circle-alert.svg?react";
+export { default as CircleCheckIcon } from "./ic-circle-check.svg?react";
+export { default as CircleInfoIcon } from "./ic-circle-info.svg?react";
+export { default as CircleDeleteIcon } from "./ic-circle-delete.svg?react";
+export { default as CheckIcon } from "./ic-check.svg?react";
