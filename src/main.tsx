4f807fb8bb53a66fa71141b210438dd623a54cdf
nabbang6 <hawwah03@gachon.ac.kr>
2026-04-14 16:21:50 +0900

    feat: 디자인 토큰 및 타이포그래피 유틸리티 정의

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import "./index.css";
 import { QueryProvider } from "@/providers/query-provider";
+import { Toaster } from "@/components/ui";
 import { routes } from "@/routes";
 
 const router = createBrowserRouter(routes);
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryProvider>
       <RouterProvider router={router} />
+      <Toaster />
     </QueryProvider>
   </React.StrictMode>,
 );
