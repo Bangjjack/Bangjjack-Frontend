e1f07f0e25e121828beade045920b9f9a8131655
hdg0116 <hdg0116@gmail.com>
2026-04-20 16:25:26 +0900

    feat: 룸메이트 요청 수락 모달 UI 구현 (#12)

--- src/features/chat/components/ChatRoommateRequestMessage.tsx
+++ src/features/chat/components/ChatRoommateRequestMessage.tsx
@@ -1,30 +1,69 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogIcon,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+  Button,
+} from "@/components/ui";
 import { CheckIcon } from "@/assets/icons";
-import { Button } from "@/components/ui";
 
 export type ChatRoommateRequestMessageProps = {
-  onConfirm?: () => void;
+  onAccept?: () => void;
+  onReject?: () => void;
   requesterName: string;
 };
 
-function ChatRoommateRequestMessage({ onConfirm, requesterName }: ChatRoommateRequestMessageProps) {
+function ChatRoommateRequestMessage({
+  onAccept,
+  onReject,
+  requesterName,
+}: ChatRoommateRequestMessageProps) {
   return (
-    <div className="flex max-w-60 flex-col justify-center gap-300 overflow-hidden rounded-tl-2xl rounded-tr-2xl rounded-br-2xl border border-brand-primary bg-bg-secondary px-600 py-400">
-      <div className="flex items-center gap-100">
-        <CheckIcon className="size-3 text-brand-primary [&_path]:stroke-current" />
-        <span className="typo-title4 text-text-primary-alternative">룸메이트 요청</span>
+    <AlertDialog>
+      <div className="flex max-w-60 flex-col justify-center gap-300 overflow-hidden rounded-tl-2xl rounded-tr-2xl rounded-br-2xl border border-brand-primary bg-bg-secondary px-600 py-400">
+        <div className="flex items-center gap-100">
+          <CheckIcon className="size-3 text-brand-primary [&_path]:stroke-current" />
+          <span className="typo-title4 text-text-primary-alternative">룸메이트 요청</span>
+        </div>
+
+        <div className="flex flex-col gap-100">
+          <p className="typo-title3 whitespace-pre-line text-text-strong">
+            {requesterName} 님이{"\n"}룸메이트를 제안했어요!
+          </p>
+          <p className="typo-caption2 text-text-caption">수락하면 룸메이트를 확정할 수 있어요</p>
+        </div>
+
+        <AlertDialogTrigger asChild>
+          <Button className="w-full cursor-pointer rounded-medium" size="sm">
+            확인하기
+          </Button>
+        </AlertDialogTrigger>
       </div>
 
-      <div className="flex flex-col gap-100">
-        <p className="typo-title3 whitespace-pre-line text-text-strong">
-          {requesterName} 님이{"\n"}룸메이트를 제안했어요!
-        </p>
-        <p className="typo-caption2 text-text-caption">수락하면 룸메이트를 확정할 수 있어요</p>
-      </div>
-
-      <Button className="w-full rounded-medium cursor-pointer" onClick={onConfirm} size="sm">
-        확인하기
-      </Button>
-    </div>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogIcon />
+          <AlertDialogTitle>룸메이트 요청을 수락할까요?</AlertDialogTitle>
+          <AlertDialogDescription className="whitespace-normal break-keep text-center">
+            수락 시 상대방에게 매칭 완료 알림이 발송됩니다
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter className="mt-500">
+          <AlertDialogCancel className="cursor-pointer" onClick={onAccept}>
+            거절하기
+          </AlertDialogCancel>
+          <AlertDialogAction className="cursor-pointer" onClick={onReject}>
+            수락하기
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
 
