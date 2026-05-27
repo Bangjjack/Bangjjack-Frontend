5256b6cb982ee2a8cb72f0ca75712efad3fc99d2
nabbang6 <hawwah03@gachon.ac.kr>
2026-05-27 23:59:30 +0900

    fix: prettier 포맷팅 수정

--- src/features/mypage/components/index.ts
+++ src/features/mypage/components/index.ts
@@ -9,7 +9,10 @@ export type { MyChecklistContentProps } from "@/features/mypage/components/check
 export { MyActivityContent } from "@/features/mypage/components/MyActivityContent";
 export type { MyActivityContentProps } from "@/features/mypage/components/MyActivityContent";
 export { BookmarkCard, MyBookmarkContent } from "@/features/mypage/components/bookmark";
-export type { BookmarkCardProps, MyBookmarkContentProps } from "@/features/mypage/components/bookmark";
+export type {
+  BookmarkCardProps,
+  MyBookmarkContentProps,
+} from "@/features/mypage/components/bookmark";
 export { MyChecklistChipSection } from "@/features/mypage/components/checklist/MyChecklistChipSection";
 export type { MyChecklistChipSectionProps } from "@/features/mypage/components/checklist/MyChecklistChipSection";
 export { MyProfileEditContent } from "@/features/mypage/components/MyProfileEditContent";
