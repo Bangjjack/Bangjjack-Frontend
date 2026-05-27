4583cfa596e0193b5aa261957a980eee869eb81a
nabbang6 <hawwah03@gachon.ac.kr>
2026-05-28 02:45:36 +0900

    feat: RecruitCardSkeleton·RoommateProfileCardSkeleton 컴포넌트 추가

--- src/features/home/components/index.ts
+++ src/features/home/components/index.ts
@@ -4,3 +4,4 @@ export { HomePageContent } from "@/features/home/components/HomePageContent";
 export type { HomePageContentProps } from "@/features/home/components/HomePageContent";
 export { OnboardingPromptSection } from "@/features/home/components/OnboardingPromptSection";
 export type { OnboardingPromptSectionProps } from "@/features/home/components/OnboardingPromptSection";
+export { RoommateProfileCardSkeleton } from "@/features/home/components/RoommateProfileCardSkeleton";
